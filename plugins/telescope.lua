8804b06796947c329e4261b136852c11f913edbe
ori92 <orieliyahu92@gmail.com>
2022-06-26 22:28:04 +0300

    live-grep is now searching in hidden files.

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,55 +1,64 @@
 local status_ok, telescope = pcall(require, "telescope")
 if status_ok then
-  local fb_actions = telescope.extensions.file_browser.actions
+	local fb_actions = telescope.extensions.file_browser.actions
 
-  return {
-    defaults = {
-      prompt_prefix = " ",
-      layout_strategy = "horizontal",
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-          preview_width = 0.55,
-          results_width = 0.8,
-        },
-        vertical = {
-          mirror = false,
-        },
-        width = 0.87,
-        height = 0.80,
-        preview_cutoff = 120,
-      },
-      mappings = {
-        i = {
-          ["<C-z>"] = "which_key",
-        },
-      },
-      file_ignore_patterns = {
-        ".git",
-        ".cache",
-      },
-    },
-    extensions = {
-      file_browser = {
-        mappings = {
-          i = {
-            ["<C-/>"] = fb_actions.toggle_hidden,
-          },
-          n = {
-            z = fb_actions.toggle_hidden,
-          },
-        },
-      },
-    },
-    pickers = {
-      find_files = {
-        hidden = true,
-      },
-      live_grep = {
-        additional_args = function(opts)
-          return { "--hidden" }
-        end,
-      },
-    },
-  }
+	return {
+		defaults = {
+			prompt_prefix = " ",
+			layout_strategy = "horizontal",
+			layout_config = {
+				horizontal = {
+					prompt_position = "top",
+					preview_width = 0.55,
+					results_width = 0.8,
+				},
+				vertical = {
+					mirror = false,
+				},
+				width = 0.87,
+				height = 0.80,
+				preview_cutoff = 120,
+			},
+			mappings = {
+				i = {
+					["<C-z>"] = "which_key",
+				},
+			},
+			file_ignore_patterns = {
+				".git",
+				".cache",
+			},
+		},
+		extensions = {
+			file_browser = {
+				mappings = {
+					i = {
+						["<C-/>"] = fb_actions.toggle_hidden,
+					},
+					n = {
+						z = fb_actions.toggle_hidden,
+					},
+				},
+			},
+		},
+		pickers = {
+			find_files = {
+				hidden = true,
+			},
+			live_grep = {
+				additional_args = function(opts)
+					return {
+						"--hidden",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--iglob",
+						"!.git",
+					}
+				end,
+			},
+		},
+	}
 end
