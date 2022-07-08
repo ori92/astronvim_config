124a4e8e2f18c1bad78b7d3a03a52b1549b12d7c
ori92 <orieliyahu92@gmail.com>
2022-07-08 08:51:56 +0300

    catppuccin removed. startify fixed.

--- plugins/init.lua
+++ plugins/init.lua
@@ -64,72 +64,11 @@ return {
     module = "telescope._extensions.zoxide",
   },
   ["goolord/alpha-nvim"] = { disable = true },
-  ["catppuccin/nvim"] = {
-    as = "catppuccin",
-    config = function()
-      vim.g.catppuccin_flavour = "mocha"
-      require("catppuccin").setup {
-        term_colors = true,
-        integrations = {
-          treesitter = true,
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = "italic",
-              hints = "italic",
-              warnings = "italic",
-              information = "italic",
-            },
-            underlines = {
-              errors = "undercurl",
-              hints = "undercurl",
-              warnings = "undercurl",
-              information = "undercurl",
-            },
-          },
-          lsp_trouble = false,
-          cmp = true,
-          lsp_saga = false,
-          gitgutter = false,
-          gitsigns = true,
-          telescope = true,
-          nvimtree = {
-            enabled = false,
-            show_root = false,
-            transparent_panel = false,
-          },
-          neotree = {
-            enabled = true,
-            show_root = true,
-            transparent_panel = false,
-          },
-          which_key = true,
-          indent_blankline = {
-            enabled = true,
-            colored_indent_levels = true,
-          },
-          dashboard = false,
-          neogit = false,
-          vim_sneak = false,
-          fern = false,
-          barbar = false,
-          bufferline = true,
-          markdown = true,
-          lightspeed = false,
-          ts_rainbow = true,
-          hop = true,
-          notify = true,
-          telekasten = false,
-          symbols_outline = false,
-        },
-      }
-    end,
-  },
   ---- My Custom
   ["mg979/vim-visual-multi"] = {}, -- visual-multi cursor
   ["machakann/vim-sandwich"] = {}, -- vim-sandwitch - surronding.
   ["AndrewRadev/sideways.vim"] = {}, -- sideways - shift parameters.
-  -- ["mhinz/vim-startify"] = {}, -- Startify.
+  ["mhinz/vim-startify"] = {}, -- Startify.
   ["sudormrfbin/cheatsheet.nvim"] = {}, -- Cheatsheet
   ["RRethy/nvim-align"] = {},
   ["s1n7ax/nvim-window-picker"] = { -- Window Picker
