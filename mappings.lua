34e41ad95db500d5edb2273a3eb148c5432f67d5
ori92 <orieliyahu92@gmail.com>
2022-07-08 08:52:20 +0300

    my custom mappings were added.

--- mappings.lua
+++ mappings.lua
@@ -12,6 +12,22 @@ local function vim_opt_toggle(user_opt, first, second)
   end
 end
 
+-- Abbreviations
+local abbreviations = {
+  Wq = "wq",
+  WQ = "wq",
+  Wqa = "wqa",
+  W = "w",
+  Q = "q",
+  Qa = "qa",
+  wrap = "set wrap",
+  nowrap = "set nowrap",
+}
+
+for left, right in pairs(abbreviations) do
+  vim.cmd(string.format("cnoreabbrev %s %s", left, right))
+end
+
 return {
   n = {
     ["<leader>q"] = { ":Bdelete!<cr>", desc = "Quit Buffer" },
@@ -185,15 +201,59 @@ return {
       end,
       desc = "Packer Search",
     },
+    -- Typing gcc is too much (comments toggle)
+    ["<C-c>"] = "<Cmd>CommentToggle<CR>",
+
+    -- Alt + Left/Right to move between tabs.
+    ["<M-Right>"] = "<Cmd>BufferLineCycleNext<CR>",
+    ["<M-Left>"] = "<Cmd>BufferLineCyclePrev<CR>",
+
+    -- Toggle Neotree
+    ["\\"] = "<Cmd>Neotree toggle<CR>",
+
+    ["-"] = "<c-x>", -- "Descrement number"
+    ["+"] = "<c-a>", -- "Increment number"
+
+    -- Duplicate a line / selection and comment out the first
+    ["<Leader>C"] = { "Ypk:CommentToggle<CR>j", desc = "Duplicate comment" },
+
+    -- Insert current date
+    ["<Leader>i"] = { '"=strftime("%b %d, %Y")<CR>p', desc = "insert date " },
+
+    -- Select panes (tmux integration)
+    ["<Leader><Up>"] = "<cmd>lua require('tmux').move_top()<cr>",
+    ["<Leader><Down>"] = "<cmd>lua require('tmux').move_bottom()<cr>",
+    ["<Leader><Left>"] = "<cmd>lua require('tmux').move_left()<cr>",
+    ["<Leader><Right>"] = "<cmd>lua require('tmux').move_right()<cr>",
+
+    -- Disable ex mode mapping
+    ["Q"] = "<Nop>",
+    ["q:"] = "<Nop>", -- not working
+
+    -- Cheatsheet
+    ["<Leader>?"] = "<Cmd>Cheatsheet<CR>",
+
+    -- <esc> to clear search highlight
+    ["<esc>"] = "<Cmd>nohlsearch<CR>",
   },
   v = {
     ["<A-j>"] = { ":m '>+1<cr>gv=gv" },
     ["<A-k>"] = { ":m '<-2<cr>gv=gv" },
+
+    -- Move lines Up/Down using Ctrl+Arrows
+    ["<C-Up>"] = ":m '<-2<CR>gv=gv",
+    ["<C-Down>"] = ":m '>+1<CR>gv=gv",
+
+    -- Typing gcc is too much (comments toggle)
+    ["<C-c>"] = ":CommentToggle<CR>gv",
   },
   i = {
     [","] = ",<c-g>u",
     ["."] = ".<c-g>u",
     ["!"] = "!<c-g>u",
     ["?"] = "?<c-g>u",
+    -- Move to beginning/end of line while in insert mode
+    ["<C-a>"] = "<C-o>0",
+    ["<C-e>"] = "<C-o>$",
   },
 }
